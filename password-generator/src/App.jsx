6b64f2b29283a29d8e55d7f7d31ebb5bc96f3716
harshakp06 <kpharsha@yahoo.com>
2024-11-25 23:24:07 +0530

    added

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,6 +8,36 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null)
+
+  const generatePassword = useCallback(() => {
+    let pass = ""
+
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%&*()_+"
+
+    for (let i = 0; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1)
+      
+      pass += str.charAt(char) 
+      
+    }
+
+    setPassword(pass)
+  }, [length, charAllowed, numberAllowed])
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select()
+  }
+
+
+  useEffect(() => {
+    generatePassword()
+  }, [length, numberAllowed, charAllowed])
 
   return (
     <>
@@ -20,8 +50,12 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          <button 
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          onClick={copyPasswordToClipboard}
+          >
             Copy
           </button>
         </div>
@@ -48,7 +82,7 @@ function App() {
               onChange={() => {
                 setNumberAllowed((prev) => !prev);
               }}
-              
+             
             />
             <label htmlFor="number">Numbers</label>
           </div>
